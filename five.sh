22eab2ad5098f19bc1afa248450038073edae021
koleks92 <koleks92@gmail.com>
2023-09-03 21:29:46 +0200

    Upgradet version

--- five.sh
+++ five.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 # Program to run my other four programs
-./questionnaire.sh
-./countdown.sh 3
-./bingo.sh
-./fortune.sh
+echo -e "\n"~~ Program runner ~~"\n"
+echo 1. Questionnaire
+echo 2. Bingo
+echo 3. Countdown 5
+echo -e 4. Fortune"\n"
+echo "Please choose a number: "
+read CHOICE
+until (( CHOICE == 1 || CHOICE == 2 || CHOICE == 3 || CHOICE == 4))
+do
+    echo "Please choose a correct number: "
+    read CHOICE
+done
+
+if (( CHOICE == 1 ))
+then
+    ./questionnaire.sh
+elif (( CHOICE == 2 ))
+then
+    ./bingo.sh
+elif (( CHOICE == 3 ))
+then
+    ./countdown.sh 5
+elif (( CHOICE == 4 ))
+then
+    ./fortune.sh
+fi
